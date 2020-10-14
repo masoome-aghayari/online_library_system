7e3d695942cf7fd32e87cb4090dff9501e7e235f
aghayari <masoome.aghayari@yahoo.com>
2020-10-14 14:53:56 +0330

    adding userDtoConverter class

---
+++ src/main/java/ir/nrdc/service/converter/UserDtoConverter.java
@@ -0,0 +1,44 @@
+package ir.nrdc.service.converter;
+
+import ir.nrdc.model.dto.UserDto;
+import ir.nrdc.model.entity.Role;
+import ir.nrdc.model.entity.User;
+import ir.nrdc.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDtoConverter {
+    @Autowired
+    RoleService roleService;
+
+    public UserDto convertUserToDto(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setNationalId(user.getNationalId());
+        userDto.setAge(user.getAge());
+        userDto.setGender(user.getGender());
+        userDto.setEmail(user.getEmail());
+        userDto.setRole(user.getRole().getRoleName().name());
+        return userDto;
+    }
+
+    public User convertDtoToUser(UserDto userDto) {
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setNationalId(userDto.getNationalId());
+        user.setAge(userDto.getAge());
+        user.setGender(userDto.getGender());
+        user.setMobileNumber(userDto.getMobileNumber());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setRole(stringToRoleConverter(userDto.getRole()));
+        return user;
+    }
+
+    public Role stringToRoleConverter(String roleName) {
+        return roleService.findRoleByName(roleName);
+    }
+}
