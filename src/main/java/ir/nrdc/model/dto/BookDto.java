4bc2942f3a8884896b668d928f0d4a9b619f9c52
aghayari <masoome.aghayari@yahoo.com>
2020-10-22 01:16:51 +0330

    removing JsonProperty annotation from BookDto attributes

--- src/main/java/ir/nrdc/model/dto/BookDto.java
+++ src/main/java/ir/nrdc/model/dto/BookDto.java
@@ -9,22 +9,13 @@ import java.io.Serializable;
 @Setter
 @Getter
 public class BookDto implements Serializable {
-    @JsonProperty("name")
     private String name;
-    @JsonProperty("author")
     private AuthorDto author;
-    @JsonProperty("publisher")
     private String publisher;
-    @JsonProperty("ageGroup")
     private String ageGroup;
-    @JsonProperty("subject")
     private String subject;
-    @JsonProperty("isbn")
     private String isbn;
-    @JsonProperty("numberOfPages")
     private int numberOfPages;
-    @JsonProperty("publishDate")
     private String publishDate;
-    @JsonProperty("number")
     private int number;
 }
